941e5f39f52c4762fa1b079d05bcf8f73a7c44ba
Leroy1022 <107305750+Leroy1022@users.noreply.github.com>
2023-05-11 17:59:36 +1000

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -1,74 +1,62 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-interface Location {
-  value: string;
-  viewValue: string;
+import { Component } from '@angular/core';
+import {Sort} from '@angular/material/sort';
+
+export interface Dessert {
+  calories: number;
+  carbs: number;
+  fat: number;
+  name: string;
+  protein: number;
 }
-interface Building {
-  value: string;
-  viewValue: string;
-}
-interface provider {
-  value: string;
-  viewValue: string;
-}
-interface Account {
-  value: string;
-  viewValue: string;
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'angular-material-dialog-app';
-  @ViewChild('dialogRef')
-  dialogRef!: TemplateRef<any>;
-  myFooList = ['Some Item', 'Item Second', 'Other In Row', 'What to write', 'Blah To Do']
-  constructor(public dialog: MatDialog) { }
-
-    
-  locations: Location[] = [
-    {value: 'Manhattan0', viewValue: 'manhattan'},
-    {value: 'broklyn1', viewValue: ' Broklyn'},
-    {value: 'Philly1', viewValue: 'Philly'},
-  ];
-  buildings: Building[] = [
-    {value: 'Empire0', viewValue: 'Empire State Building'},
-    {value: 'Statue1', viewValue: ' Statue of Liberty'},
-    {value: 'AT&T', viewValue: 'AT&T Building'},
-  ];
-  Providers: provider[] = [
-    {value: 'Empire0', viewValue: 'Empire State Building'},
-    {value: 'Statue1', viewValue: ' Statue of Liberty'},
-    {value: 'AT&T', viewValue: 'AT&T Building'},
-  ];
-  accounts: Account[] = [
-    {value: 'A0', viewValue: '49-4202-3200-0000-8'},
-    {value: 'A1', viewValue: '49-4202-3200-0000-9'},
-    {value: 'A2', viewValue: '49-4202-3200-0000-10'},
+  title = 'column';desserts: Dessert[] = [
+    {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
+    {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
+    {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
+    {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
+    {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
   ];
 
-  openTempDialog() {
-    const myCompDialog = this.dialog.open(this.dialogRef, { data: this.myFooList,panelClass: 'fullscreen-dialog',
-    height: '80vh',
-    width: '150%', position:{
-      left :'20px',
-      top : '10px' 
-    } });
-    myCompDialog.afterOpened().subscribe((res) => {
-      // Trigger After Dialog Opened
-      console.log('After Opened', { res });
-    });
-    myCompDialog.beforeClosed().subscribe((res) => {
-      // Trigger Before Dialog Closed
-      console.log('Before Closed', { res });
-    });
-    myCompDialog.afterClosed().subscribe((res) => {
-      // Trigger After Dialog Closed
-      console.log('After Closed', { res });
+  sortedData: Dessert[];
+
+  constructor() {
+    this.sortedData = this.desserts.slice();
+  }
+
+  sortData(sort: Sort) {
+    const data = this.desserts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'calories':
+          return compare(a.calories, b.calories, isAsc);
+        case 'fat':
+          return compare(a.fat, b.fat, isAsc);
+        case 'carbs':
+          return compare(a.carbs, b.carbs, isAsc);
+        case 'protein':
+          return compare(a.protein, b.protein, isAsc);
+        default:
+          return 0;
+      }
     });
   }
 }
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
+
--- app.module.ts
+++ app.module.ts
@@ -1,32 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSortModule} from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatCardModule,
-    MatExpansionModule,
-    MatInputModule,
-    MatDatepickerModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
